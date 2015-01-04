26245482b9baef8a17bc645f505f54653b2d07bf
denny <wanziforever>
2015-01-04 10:56:52 +0800

    asf

--- mkfiles/lib/timer/.TMprivate.d
+++
@@ -1,6 +0,0 @@
-TMprivate.o: ../../../cc/lib/tim/TMprivate.cc ../../../hdr/GLtypes.h \
- ../../../hdr/GLreturns.h ../../../hdr/GLmsgs.h \
- ../../../cc/hdr/tim/TMtimers.hh ../../../cc/hdr/linux/synch.h \
- ../../../cc/hdr/linux/futex/usersem.h \
- ../../../cc/hdr/linux/futex/x86_64.h ../../../cc/hdr/tim/TMreturns.hh \
- ../../../cc/lib/tim/TMlocal.hh
--- mkfiles/lib/timer/.TMtimers.d
+++
@@ -1,5 +0,0 @@
-TMtimers.o: ../../../cc/lib/tim/TMtimers.cc ../../../hdr/GLtypes.h \
- ../../../cc/hdr/tim/TMtimers.hh ../../../cc/hdr/linux/synch.h \
- ../../../cc/hdr/linux/futex/usersem.h \
- ../../../cc/hdr/linux/futex/x86_64.h ../../../hdr/GLreturns.h \
- ../../../cc/hdr/tim/TMreturns.hh ../../../cc/lib/tim/TMlocal.hh
